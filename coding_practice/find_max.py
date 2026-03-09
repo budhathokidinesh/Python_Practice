1e3d6411b77d675981eca40b07a5e3eccb602b02
Dinesh Budhathoki <physmarika@gmail.com>
2026-03-09 12:54:57 +0800

    practicing find max and min

---
+++ coding_practice/find_max.py
@@ -0,0 +1,11 @@
+# ths is for finding max number
+def find_max(nums):
+    return max(nums)
+
+print(find_max([1,2,3]))
+
+# this is for finding min number 
+def find_min(nums):
+    return min(nums)
+
+print(find_min([3, 5,7,2]))
